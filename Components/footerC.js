8a1e5d4841aba16e337d733676b00cedecbd911f
gk072745 <kumawatgovind222@gmail.com>
2022-09-29 07:31:03 +0530

    day-1 tasks added

---
+++ Components/footerC.js
@@ -0,0 +1,138 @@
+
+
+let footer1=()=>{
+    return ` 
+    <div id="footer1_1_1">
+        <p>About Tripadvisor</p>
+        <a href="">About Us</a>
+        <a href="">Press</a>
+        <a href="">Resources and Policies</a>
+        <a href="">Careers</a>
+        <a href="">Trust & Safety</a>
+        <a href="">Contact us</a>
+        <a href="">Accessibility Statement</a>
+    </div>
+    <div id="footer1_1_2">
+        <p>Explore</p>
+        <a href="">Write a review</a>
+        <a href="">Add a Place</a>
+        <a href="">Join</a>
+        <a href="">Travellers' Choice</a>
+        <a href="">GreenLeaders</a>
+        <a href="">Help Centre</a>
+        <a href="">Travel Articles</a>
+    </div>
+    <div id="footer1_1_3">
+        <p>Do Business With Us</p>
+        <a href="">Owners & DMO/CVB</a>
+        <a href="">Business Advantage</a>
+        <a href="">Sponsored Placements</a>
+        <a href="">Access our Content API</a>
+     <p>Get The App</p>
+       <a href="">iPhone App</a>
+       <a href="">Android App</a>
+    </div>
+    <div id="footer1_1_4">
+        <p>Tripadvisor Sites</p>
+        <p>Book tours and attraction tickets on<a href="">Viator</a></p> 
+    </div>`
+}
+
+let footer2=()=>{
+    return`  <div id="footer1_2_1">
+    <div id="footer1_2_1_1"><img src="https://static.tacdn.com/img2/brand_refresh/Tripadvisor_logoset_solid_green.svg" alt=""></div>
+    <div id="footer1_2_1_2">
+         <p id="footer1_2_1_2_1">© 2022 Tripadvisor LLC All rights reserved.</p>
+         <div id="footer1_2_1_2_2">
+          <a href=""> Terms of Use</a>
+          <a href="">Privacy and Cookies Statement</a>
+          <a href="">Cookie consent</a>
+          <a href="">Site Map</a>
+          <a href="">How the site works</a>
+          <a href="">Contact us</a>  
+         </div>
+    </div>
+      </div>
+       <div id="footer1_2_2">
+           <select   name="" id="footer1_2_2_1">
+           <option value="">&#36; Dollar</option>
+           <option value="">&euro; Euro</option>
+           <option value="">&#162; Cent</option>
+           <option value="">&#163; Pound</option>
+           <option value="">&curren; G.C.</option>
+           <option value="">&yen; Yen</option>
+           <option value="">&#8355; Franc</option>
+           <option value="">&#8356; Lira</option>
+           <option value="">&#8359; Peseta</option>
+           <option value="">&#x20B9; Rupee</option>
+           <option value="">&#8361; Won </option>
+           <option value="">&#8372; Hvyvnia</option>
+           <option value="">&#8367; Drachma</option>
+           <option value="">&#8366; Tugrik</option>
+           <option value="">&#8368; German Penny</option>
+           <option value="">&#8370; Guarani</option>
+           <option value="">&#8369; Pesa</option>
+           <option value="">&#8371; Austral</option>
+           <option value="">&#8373; Cedi</option>
+           <option value="">&#8365; Kip</option>
+           <option value="">&#8362; New Sheqel</option>
+           <option value="">&#8363; Dong</option>
+            
+          </select>
+           <select  name="" id="footer1_2_2_2">
+            <option value=""> India </option>
+            <option value=""> Shqipëria </option>
+            <option value=""> Dzayer </option>
+            <option value=""> Andorra </option>
+            <option value=""> Angola </option>
+            <option value=""> Australia </option>
+            <option value=""> বাংলাদেশ </option>
+            <option value=""> Bielaruś </option>
+            <option value=""> འབྲུག་ཡུལ </option>
+            <option value=""> بروني </option>
+            <option value=""> កម្ពុជា </option>
+            <option value=""> تشاد </option>
+            <option value=""> 中国 </option>
+            <option value=""> Colombia </option>
+            <option value=""> جزر القمر </option>
+            <option value=""> Česko </option>
+            <option value=""> جيبوتي Djibouti </option>
+            <option value="">	Sak'art'velo </option>
+            <option value=""> Ελλάς </option>
+            <option value=""> Magyarország </option>
+            <option value=""> Īrān </option>
+            <option value=""> Al-Yaman اليمن	</option>
+            <option value=""> Việt Nam</option>
+            <option value=""> Città del Vaticano	</option>
+            <option value=""> O‘zbekiston</option>
+            <option value=""> USA	</option>
+            <option value=""> Ukraїna	</option>
+            <option value=""> Türkiye</option>
+            <option value=""> تونس</option>
+            <option value=""> Mueang Thai</option>
+            <option value=""> Suriyah سورية</option>
+            <option value=""> Suisse,</option>
+            <option value=""> As-Sudan السودان</option>
+            <option value=""> ශ්‍රී ලංකාව</option>
+            <option value=""> España</option>
+            <option value=""> Paguot Thudän</option>
+
+           </select>
+
+          </div>
+
+          <p id="footer1_2_3">
+              This is the version of our website addressed to speakers of English in India. If you are a resident of another country or region, please select the appropriate version of Tripadvisor for your country or region in the drop-down menu. <a href="">more</a>
+          </p>
+
+          <div id="footer1_2_4">
+              <i class="ri-facebook-circle-fill"></i>
+              <i class="ri-twitter-fill"></i>
+              <i class="ri-pinterest-fill"></i>
+              <i class="ri-instagram-fill"></i>
+          </div>
+`
+
+}
+
+export {footer1,footer2}
